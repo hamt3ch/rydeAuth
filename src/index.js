56b02eff340005464949f7ad6e30b02395fa7191
hamt3ch <hmiles23@ufl.edu>
2017-02-08 00:23:58 -0500

    move to express frameworkd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-export default (req, res) => 'Welcome to micro'
+import http from 'http';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import config from './config.json';
+
+const app = express();
+app.server = http.createServer(app);
+
+// Logger for HTTP Request
+app.use(morgan('combined'));
+
+// 3rd party middleware
+app.use(cors({
+  exposedHeaders: config.corsHeaders,
+}));
+
+app.use(bodyParser.json({
+  limit: config.bodyLimit,
+}));
+
+// internal middleware
+// app.use(middleware({ config, db }));
+
+// api router
+// app.use('/api', api({ config, db }));
+
+app.server.listen(process.env.PORT || config.port);
+
+console.log(`Started on port ${app.server.address().port}`);
+
+export default app;
