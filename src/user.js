a4ea265457c2e27faae3af021cb9a10294ddc9c3
psdewar2 <psdewar2@live.com>
2017-03-28 17:45:16 -0700

    Implemented bcrypt-nodejs to load a hashed password before logging in

--- src/user.js
+++ src/user.js
@@ -1,11 +1,19 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt-nodejs';
 
 const user = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  currentLocation: String,
-  trips: [], // array of rideIDs
+  email: { type: String, required: true },
+  password: { type: String, required: true },
 });
+
+user.methods.validPassword = (password, hash, done) => {
+  bcrypt.compare(password, hash, (err, samePassword) => {
+    if (samePassword === true) {
+      done(null, true);
+    } else {
+      done(null, false);
+    }
+  });
+};
 
 export default mongoose.model('User', user);
